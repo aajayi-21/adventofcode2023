ab14dd845da6e78f7f684f0f0cce87e68168497e
Adeolu Ajayi <aoa2160@columbia.edu>
2024-01-08 15:21:42 -0600

    Completed day1 part1

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+
+namespace day1
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new("C:\\Users\\super\\source\\repos\\adventofcode2023\\day1\\input");
+                string line;
+                double sum = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (char.IsDigit(line[i])){
+                            sum += char.GetNumericValue(line[i]) * 10;
+                            break;
+                        }
+                    }
+
+                    for (int i = line.Length - 1; i >= 0; i--)
+                    {
+                        if (char.IsDigit(line[i])){
+                            sum += char.GetNumericValue(line[i]);
+                            break;
+                        }
+                    }
+                   
+                }
+                Console.WriteLine(sum);
+
+            }
+            catch {
+                Console.WriteLine("Error Occured");
+            }
+        }
+    }
+}
